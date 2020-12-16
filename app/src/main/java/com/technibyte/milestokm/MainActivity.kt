ef61e420a406c0849f0b2e461a57417546b81e2f
aleangelozi <aleangelozi@gmail.com>
2020-12-15 21:12:17 -0300

    Experimental commit.

--- app/src/main/java/com/technibyte/milestokm/MainActivity.kt
+++ app/src/main/java/com/technibyte/milestokm/MainActivity.kt
@@ -2,18 +2,20 @@ package com.technibyte.milestokm
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.technibyte.milestokm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val text = "Experimental"
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
